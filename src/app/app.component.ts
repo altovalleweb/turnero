1d269e77cad3d1c24aeb867dbd4e6f22301f1255
matias <mmunoznqn@gmail.com>
2022-01-19 13:17:24 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selected!: Date | null;
   title = 'turnero';
+
+  minDate = new Date(2022, 1, 9);
+  maxDate = new Date(2022, 1, 13);
+
+  horariosDisponibles=[{horario:'15:00 PM'},
+  {horario:'15:30 PM'},
+  {horario:'16:00 PM'},
+  {horario:'16:30 PM'},
+  {horario:'17:00 PM'},
+  {horario:'17:30 PM'},
+  {horario:'18:00 PM'},
+  {horario:'18:30 PM'},
+  {horario:'19:00 PM'},
+  {horario:'19:30 PM'},
+  {horario:'20:00 PM'},
+  {horario:'20:30 PM'}]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter'
+import {MatCardModule} from '@angular/material/card'
+import { registerLocaleData } from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -10,9 +23,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+  MatMomentDateModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
